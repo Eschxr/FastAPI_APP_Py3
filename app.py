a5c48c14ddcfb3f6ce55be7f87c5b799bd96837d
Escher <46295107+Eschxr@users.noreply.github.com>
2023-12-07 14:03:52 +0000

    added functionality to app

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 # This app uses Laurence's API to give users information on a variety of food items. 
+# PLEASE RERUN THE APP TO USE IT AGAIN.
 
 
 import requests
 
+url = "https://192.168.1.29:8000/"
+
+usr_input = input("Welcome to the ultimate food compendium, what information may you seek today? \n a -- a full list of all available foods \n b -- specific info on one food only (PLEASE PROVIDE THE NAME WITH THE FIRST LETTER CAPITALIZED) \n > ")
+
+
+if usr_input == "a":
+    source = "https://192.168.1.29:8000/foodname?api_key=alicealice"
+    response = requests.get(url=source)
+    data = response.json()
+    print(data)
+
+elif usr_input == "b":
+    prompt = input("What food would you like to view? \n> ")
+    source = f"https://192.168.1.29:8000/foodname/{prompt}?api_key=alicealice"
+    response = requests.get(url=source)
+    data = response.json()
+    print(data)
